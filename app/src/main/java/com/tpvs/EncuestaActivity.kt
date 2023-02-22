6a41ac21f2b7336c6025d4465671d580bc22d41c
erick ocampo <erickocampocortez@gmail.com>
2023-02-22 13:36:17 -0600

    camera functionality

--- app/src/main/java/com/tpvs/EncuestaActivity.kt
+++ app/src/main/java/com/tpvs/EncuestaActivity.kt
@@ -1,17 +1,17 @@
 package com.tpvs
 
 import android.app.Activity
-import android.graphics.Bitmap
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.result.ActivityResult
-import androidx.annotation.RequiresApi
-import android.os.Build
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 
 class EncuestaActivity : AppCompatActivity() {
 
@@ -20,15 +20,22 @@ class EncuestaActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_encuesta)
 
-        val btnCam1 = findViewById<Button>(R.id.btnCam1)
+        val btnCam1 = findViewById<Button>(R.id.btnCam2)
+        val btnCam2 = findViewById<Button>(R.id.btnCam1)
+        val btnCam3 = findViewById<Button>(R.id.btnCam3)
 
-
+        //Evento al presionar el botón
         btnCam1.setOnClickListener {
             startForResult.launch(Intent(MediaStore.ACTION_IMAGE_CAPTURE))
+
         }
+
     }
-    private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-                result: ActivityResult ->
+
+
+    //Evento que procesa el resultado de la cámara y envía la vista previa de la foto al ImageView
+    private val startForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
             if (result.resultCode == Activity.RESULT_OK) {
                 val intent = result.data
                 val imageBitmap = intent?.extras?.get("data") as Bitmap
