b6c1763bbf2217626375fec9f4207a1688a1ae80
erick ocampo <erickocampocortez@gmail.com>
2023-02-27 21:49:31 -0600

    pantalla activity_encuesta_main

--- app/src/main/java/com/tpvs/EncuestaMainActivity.kt
+++ app/src/main/java/com/tpvs/EncuestaMainActivity.kt
@@ -4,33 +4,43 @@ import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
-import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class EncuestaActivity : AppCompatActivity() {
+class EncuestaMainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapClickListener,
+    GoogleMap.OnMapLongClickListener {
 
-    private lateinit var map: GoogleMap
+    var txtLatitud: EditText? = null
+    var txtLongitud: EditText? = null
+    var mMap: GoogleMap? = null
 
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_encuesta)
-
-
+        setContentView(R.layout.activity_encuesta_main)
 
         val btnCam1 = findViewById<Button>(R.id.btnCam1)
         val btnCam2 = findViewById<Button>(R.id.btnCam2)
         val btnCam3 = findViewById<Button>(R.id.btnCam3)
-
-
+        txtLatitud = findViewById(R.id.txtLatitud)
+        txtLongitud = findViewById(R.id.txtLongitud)
+        //mapa
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment!!.getMapAsync(this)
+        //camara
         btnCam1.setOnClickListener {
             startForResult.launch(Intent(MediaStore.ACTION_IMAGE_CAPTURE))
             btnCam2.setOnClickListener {
@@ -68,9 +78,38 @@ class EncuestaActivity : AppCompatActivity() {
                 val imageView = findViewById<ImageView>(R.id.imageView3)
                 imageView.setImageBitmap(imageBitmap)
             }
-        }
 
 
+    }
 
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        mMap!!.setOnMapClickListener(this)
+        mMap!!.setOnMapLongClickListener(this)
 
+        val mexico = LatLng(19.8077463, -99.4077038)
+        mMap!!.addMarker(MarkerOptions().position(mexico).title("México"))
+        mMap!!.moveCamera(CameraUpdateFactory.newLatLng(mexico))
+    }
+
+    override fun onMapClick(latLng: LatLng) {
+        txtLatitud?.setText(latLng.latitude.toString())
+        txtLongitud?.setText(latLng.longitude.toString())
+
+        mMap!!.clear()
+        val mexico = LatLng(latLng.latitude, latLng.longitude)
+        mMap!!.addMarker(MarkerOptions().position(mexico).title(""))
+        mMap!!.moveCamera(CameraUpdateFactory.newLatLng(mexico))
+    }
+
+    override fun onMapLongClick(latLng: LatLng) {
+        txtLatitud?.setText(latLng.latitude.toString())
+        txtLongitud?.setText(latLng.longitude.toString())
+
+        mMap!!.clear()
+        val mexico = LatLng(latLng.latitude, latLng.longitude)
+        mMap!!.addMarker(MarkerOptions().position(mexico).title(""))
+        mMap!!.moveCamera(CameraUpdateFactory.newLatLng(mexico))
+    }
 }
+
---
+++ app/src/main/java/com/tpvs/MapActivity.kt
@@ -0,0 +1,59 @@
+package com.tpvs
+
+import android.os.Bundle
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+class MapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapClickListener,
+    GoogleMap.OnMapLongClickListener {
+
+    var txtLatitud: EditText? = null
+    var txtLongitud:EditText? = null
+    var mMap: GoogleMap? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_encuesta_main)
+
+        txtLatitud = findViewById(R.id.txtLatitud)
+        txtLongitud = findViewById(R.id.txtLongitud)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment!!.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        mMap!!.setOnMapClickListener(this)
+        mMap!!.setOnMapLongClickListener(this)
+
+        val mexico = LatLng(19.8077463, -99.4077038)
+        mMap!!.addMarker(MarkerOptions().position(mexico).title("México"))
+        mMap!!.moveCamera(CameraUpdateFactory.newLatLng(mexico))
+    }
+
+    override fun onMapClick(latLng: LatLng) {
+        txtLatitud?.setText(latLng.latitude.toString())
+        txtLongitud?.setText(latLng.longitude.toString())
+
+        mMap!!.clear()
+        val mexico = LatLng(latLng.latitude, latLng.longitude)
+        mMap!!.addMarker(MarkerOptions().position(mexico).title(""))
+        mMap!!.moveCamera(CameraUpdateFactory.newLatLng(mexico))
+    }
+
+    override fun onMapLongClick(latLng: LatLng) {
+//        txtLatitud?.setText(latLng.latitude.toString())
+//        txtLongitud?.setText(latLng.longitude.toString())
+//
+//        mMap!!.clear()
+//        val mexico = LatLng(latLng.latitude, latLng.longitude)
+//        mMap!!.addMarker(MarkerOptions().position(mexico).title(""))
+//        mMap!!.moveCamera(CameraUpdateFactory.newLatLng(mexico))
+    }
+}
